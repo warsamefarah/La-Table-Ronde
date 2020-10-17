d51b83ffb8c4848ab9aa6b57ffc4be6d9065f62e
Warsamé <warsame.farah_abdi@edu.devinci.fr>
2020-10-18 01:30:58 +0200

    :dizzy: added nav icons and updated nav animations

--- script.js
+++
@@ -1,9 +0,0 @@
-document.onreadystatechange = function(){
-    if (document.readyState == "complete") {
-        const navchk = document.querySelector('nav')
-
-        navchk.addEventListener('click', () => {
-            navchk.classList.toggle('show-nav')
-        })
-    }
-}
