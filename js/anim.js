6bf6476cfc353be64e5e8aa160b4b0c26b71ebe4
Rivière Mathis <mathis.riviere@edu.devinci.fr>
2021-04-25 16:23:06 +0200

    added chapter 5 and  6

--- js/anim.js
+++ js/anim.js
@@ -67,6 +67,32 @@ $(document).ready(function() {
         if ($('input[name=quinze]').is(':checked')) {
             gsap.to('.art15', { opacity: 1, duration: 0.2, y: -50 })
         }
+        if ($('input[name=seize]').is(':checked')) {
+            gsap.to('.art16', { opacity: 1, duration: 0.2, y: -50 })
+        }
+
+        if ($('input[name=dixsept]').is(':checked')) {
+            gsap.to('.art17', { opacity: 1, duration: 0.2, y: -50 })
+        }
+
+        if ($('input[name=dixhuit]').is(':checked')) {
+            gsap.to('.art18', { opacity: 1, duration: 0.2, y: -50 })
+        }
+
+        if ($('input[name=dixneuf]').is(':checked')) {
+            gsap.to('.art19', { opacity: 1, duration: 0.2, y: -50 })
+        }
+        if ($('input[name=vingt]').is(':checked')) {
+            gsap.to('.art20', { opacity: 1, duration: 0.2, y: -50 })
+        }
+
+        if ($('input[name=vingtetun]').is(':checked')) {
+            gsap.to('.art21', { opacity: 1, duration: 0.2, y: -50 })
+        }
+
+        if ($('input[name=vingtdeux]').is(':checked')) {
+            gsap.to('.art22', { opacity: 1, duration: 0.2, y: -50 })
+        }
 
     });
 
@@ -80,14 +106,16 @@ $(document).ready(function() {
         TweenMax.fromTo('.line5', { scaleX: 0, transformOrigin: '0 105%' }, { scaleX: 1, duration: 1, delay: 1.4, ease: 'power1.inOut' });
         TweenMax.fromTo('.line6', { scaleX: 0, transformOrigin: '0 105%' }, { scaleX: 1, duration: 1, delay: 1.4, ease: 'power1.inOut' });
         TweenMax.fromTo('.line7', { scaleX: 0, transformOrigin: '0 105%' }, { scaleX: 1, duration: 1, delay: 1.4, ease: 'power1.inOut' });
+        TweenMax.fromTo('.line8', { scaleX: 0, transformOrigin: '0 105%' }, { scaleX: 1, duration: 1, delay: 1.4, ease: 'power1.inOut' });
+        TweenMax.fromTo('.line9', { scaleX: 0, transformOrigin: '0 105%' }, { scaleX: 1, duration: 1, delay: 1.4, ease: 'power1.inOut' });
     })
 
     $(document).click(function(e) {
         if (!$(e.target).hasClass('dot')) {
             $('input[type=radio]').prop("checked", false);
             $('.dot').removeClass('dot-color');
-            gsap.to('.art2', 'art5', '.art7', 'art8', 'art9', 'art12', 'art14', { opacity: 0, duration: 0.2, y: -50 })
-            gsap.to('.art3, .art1', 'art4', 'art6', 'art10', 'art11', 'art13', 'art15', { opacity: 0, duration: 0.2, y: 50 })
+            gsap.to('.art2', 'art5', '.art7', 'art8', 'art9', 'art12', 'art14', 'art16', 'art19', { opacity: 0, duration: 0.2, y: -50 })
+            gsap.to('.art3, .art1', 'art4', 'art6', 'art10', 'art11', 'art13', 'art15', 'art17', 'art18', 'art20', 'art21', 'art22', { opacity: 0, duration: 0.2, y: 50 })
         }
     });
 });
